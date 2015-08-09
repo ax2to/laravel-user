99c5f0a35376aaf7c2bcc055fb75d460480a3cd5
Alan Tello Oyola <alan.tello@gmail.com>
2015-08-08 19:08:30 -0500

    Update register view
    Update form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -22,10 +22,26 @@
                             <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
                             <div class="form-group">
-                                <label class="col-md-4 control-label">Name</label>
+                                <label class="col-md-4 control-label">First Name</label>
 
                                 <div class="col-md-6">
                                     <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Last Name</label>
+
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control" name="lastname" value="{{ old('lastname') }}">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Username</label>
+
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control" name="username" value="{{ old('username') }}">
                                 </div>
                             </div>
 
